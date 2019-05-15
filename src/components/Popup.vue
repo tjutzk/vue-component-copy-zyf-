82bf854f800c18f278aa9ae7daa5e86c588c1427
tjutzk <1356476048@qq.com>
2019-05-15 18:28:30 +0800

    初始化

---
+++ src/components/Popup.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        这是popup
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
+
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        主页
+        <Popup></Popup>
+    </div>
+</template>
+<script>
+import Popup from "../components/Popup"
+export default {
+    components:{
+        Popup
+    }
+}
+</script>
+
